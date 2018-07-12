f62aa67d038bdf4261bd42bcf93f0156ed9c5031
Hassan A <has_is_ere@hotmail.com>
2018-07-12 16:16:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function updateObjectWithKeyAndValue(obj, key, value){
 
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
   obj[key] = value
+  return obj
 }
 
 function deleteFromObjectByKey(object, key){
