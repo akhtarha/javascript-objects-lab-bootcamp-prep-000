6dd3d79310b7be1df95251a8887be1681472cf44
Hassan A <has_is_ere@hotmail.com>
2018-07-12 16:53:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
   return obj
 }
 
-function deleteFromObjectByKey(object, key){
-  
+function deleteFromObjectByKey(obj, key){
+  var new_obj = Object.assign({}, obj)
+  delete new_obj[key]
+  return new_obj
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
